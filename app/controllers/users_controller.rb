0e7db9dac2883c054bbec0d6693ace7bc699ffef
activeliang <activeliang@gmail.com>
2018-08-23 20:50:51 +0800

    refactor login logout page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,22 @@
 class UsersController < ApplicationController
   def new
-  @user = User.new
-end
-
-def create
-  @user =User.new(user_params)
-  if @user.save && login(params[:user][:phone], params[:user][:password], true)
-    flash[:notice] = '注册成功～'
-    remember_me!
-    redirect_to root_path
-  else
-    @user.errors
-    render action: :new
+    @user = User.new
   end
-end
 
-private
+  def create
+  @user =User.new(user_params)
+    if @user.save && login(params[:user][:phone], params[:user][:password], true)
+      flash[:notice] = '注册成功～'
+      remember_me!
+      redirect_to root_path
+    else
+      @user.errors
+      render action: :new
+    end
+  end
 
-def user_params
-  params.require(:user).permit(:password, :password_confirmation, :phone, :token)
-end
+  private
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation, :phone, :token)
+    end
 end
